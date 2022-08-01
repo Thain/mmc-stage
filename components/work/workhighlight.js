0509276fdd2d28cc790bdde99717ff4e22355bc9
Thain <lwalkerchung@gmail.com>
2022-07-31 20:52:52 -0400

    remodel work page

--- components/work/workhighlight.js
+++ components/work/workhighlight.js
@@ -1,15 +1,15 @@
 import React from "react"
 import Link from "next/link"
 
-const WorkHighlight = ( proj ) => {
-  const url = "/work/" + proj.ext;
+const WorkHighlight = ({ proj }) => {
+  const url = "/work/" + proj.path;
   return (
       <div className="highlight-card">
           <a href={url}>
-          <div><img src={proj.img} /></div>
+          <div><img src={proj.portfolio.thumb} /></div>
           </a>
           <div>
-              {proj.blurb}
+            <h5 dangerouslySetInnerHTML={{__html: proj.portfolio.blurb }} />
             <a href={url} className="arrow-link"><em>View project</em></a>
           </div>
       </div>
--- pages/work/index.js
+++ pages/work/index.js
@@ -1,108 +1,160 @@
 import React from "react"
-import content from '../../assets/data/casestudies.json';
-import { Layout, Nav, Footer, Spacer } from "../../components/meta"
-import WorkCard from "../../components/work/workcard"
-import { Intro, Challenge, Approach, Execution, Outcome,
-         LR, LRvid, LRplayer,
-         DotStrip,
-         HeaderVid, HeaderImg, GalleryVid, GalleryPlayer,
-         TwoImg, ThreeImg
-       } from "../../components/casestudy"
+import { Layout, Spacer } from "../../components/meta"
+import content from './casestudies.json';
 
+import WorkCard      from "../../components/work/workcard.js"
+import WorkHighlight from "../../components/work/workhighlight.js"
 
-export default function Page( c ) {
+export default function Work ({ }) {
 
-    const cs = c.page;
-    if(typeof(cs) == 'undefined') return ( <> <p> ERROR </p> </> )
-    else return (
-    <>
-        <div className="case-study-content">
-            <div className="case-study-head">
-                <Nav isBlack={cs.isBlack} seo={cs.seo} active="work"/>
-                { cs.header.type == "image" &&
-                    <HeaderImg src={cs.header.media} />
-                }
-                { cs.header.type == "video" &&
-                    <HeaderVid src={cs.header.media} />
-                }
-            </div>
+  const seo = {
+    title: "Work",
+    desc: ""
+  }
 
-            <div className="case-study-intro reg-w">
-                <div>
-                    <h4 className="h4p" style={{color: cs.color1, fontStyle: "italic"}}>{cs.name}</h4>
-                    <h3 className="h3s" dangerouslySetInnerHTML={{__html: cs.title}}></h3>
-                </div>
-
-                <div className="case-study-tags">
-                    <p style={{fontWeight: 500, color: cs.color1}}>Focus</p>
-                    <ul>
-                      <>{ cs.foci.map((focus, index) => { return <li key={index}>{focus}</li>; })}</>
-                    </ul>
-                    <Spacer h="10px" />
-
-                    <p style={{fontWeight: 500, color: cs.color1}}>Year</p>
-                    <ul><li> {cs.year} </li></ul>
-                    <Spacer h="10px" />
-
-                    <p style={{fontWeight: 500, color: cs.color1}}>Disciplines</p>
-                    <ul><>{ cs.disciplines.map((disc, index) => { return <li key={index}>{disc}</li>; })}</></ul>
-                </div>
-
-                <Intro text={cs.numContent.intro} color={cs.color1} />
-            </div>
-
-            <div className="full-width">
-                { cs.mainMedia.type == "image" &&
-                    <img src={cs.mainMedia.media} />
-                }
-                { cs.mainMedia.type == "video" &&
-                    <HeaderVid src={cs.mainMedia.media} />
-                }
-            </div>
-
-            <Challenge text={cs.numContent.challenge} color={cs.color1} />
-            <DotStrip text={cs.numContent.dotstrip} color={cs.color2} />
-            <Approach text={cs.numContent.approach} color={cs.color1} />
-
-            <div className="full-width">
-                <>{ cs.gallery.map((gal, index) => {
-                    return gallery(gal);
-                })}</>
-            </div>
-
-            <Execution text={cs.numContent.execution} color={cs.color1} />
-
-            <div className="case-study-LR">
-                <>{ cs.LR.map((lr, index) => { return <LR key={index} color={cs.color1} LR={lr} /> })}</>
-            </div>
-
-            <Outcome text={cs.numContent.outcome} color={cs.color1} />
-            <Spacer h="80px" />
-
+  const projs = {
+    mada: {
+      img: "https://res.cloudinary.com/mymediacreative/image/upload/c_scale,w_1000/v1645638116/work/MADA_v1_bn37vi.png",
+      link: "mada",
+      blurb: <><h5>Raising $2 million to <br/>fight hunger with <em className="dorange">MADA.</em></h5></>
+    },
+    sh: {
+      img: "https://res.cloudinary.com/mymediacreative/image/upload/c_scale,w_1000/v1645638115/work/SH_v1_bmrlpn.png",
+      link: "sh",
+      blurb: <><h5>Making innovation feel safe<br/> for young parents with the <br/><em className="dorange">Montreal Children’s Hospital.</em></h5></>
+    },
+    seize: {
+      img: "https://res.cloudinary.com/mymediacreative/image/upload/c_scale,w_1000/v1645638115/work/SEIZE_v1_wazmkl.png",
+      link: "seize",
+      blurb: <><h5>Activating 4000+ students to <br/>fund a new economy with <em className="dorange">SEIZE.</em></h5></>
+    },
+    aisb: {
+      img: "https://res.cloudinary.com/mymediacreative/image/upload/c_scale,w_1000/v1645638115/work/AisB_v1_naf9gb.png",
+      link: "aisb",
+      blurb: <><h5>Educating and mobilizing young<br/> voters with <em className="dorange">Apathy is Boring.</em></h5></>
+    },
+    infaque: {
+      img: "https://res.cloudinary.com/mymediacreative/image/upload/c_scale,w_1000/v1645638115/work/Infaque_v1_rdysby.png",
+      link: "infaque",
+      blurb: <><h5>Bringing giving into the 21st<br/> century with <em className="dorange">Infāque.</em></h5></>
+    },
+    yic: {
+      img: "https://res.cloudinary.com/mymediacreative/image/upload/c_scale,w_1000/v1645638115/work/YIC_v1_gy4chm.png",
+      link: "yic",
+      blurb:  <><h5>Getting 600+ young<br/> entrepreneurs to pitch their <br/>big idea with <em className="dorange">InvestEco</em> and<br/> <em className="dorange">Renewal Funds.</em></h5></>
+    },
+    cera: {
+      name: "CERA",
+      img: "https://res.cloudinary.com/mymediacreative/image/upload/c_scale,w_700/v1645733959/work/grid/CERA_Header_xyiid0.png",
+      link: "cera",
+      tag: "Video",
+    },
+    psystem: {
+      name: "Psystem",
+      img: "https://res.cloudinary.com/mymediacreative/image/upload/c_scale,w_700/v1645638142/work/grid/Psystem_Header_aiwhko.png",
+      link: "psystem",
+      tag: "Branding",
+    },
+    mcconnell: {
+      name: "McConnell",
+      img: "https://res.cloudinary.com/mymediacreative/image/upload/c_scale,w_700/v1645638141/work/grid/McConnell_Header_qe5lva.png",
+      link: "mcconnell",
+      tag: "Video",
+    },
+    chasm: {
+      name: "CHASM",
+      img: "https://res.cloudinary.com/mymediacreative/image/upload/c_scale,w_700/v1645638141/work/grid/CHASM_Header_ghro3j.png",
+      link: "chasm",
+      tag: "Website",
+    },
+    ECTHP: {
+      name: "The Hunger Project",
+      img: "https://res.cloudinary.com/mymediacreative/image/upload/c_scale,w_700/v1645733960/work/grid/EthicalCoach_Header_ufw90l.png",
+      link: "ECTHP",
+      tag: "Graphic Design",
+    },
+    nofish: {
+      name: "NoFish",
+      img: "https://res.cloudinary.com/mymediacreative/image/upload/c_scale,w_700/v1645733960/work/grid/NoFish_Header_umrpak.png",
+      link: "nofish",
+      tag: "Campaign Strategy",
+    },
+    pop: {
+      name: "POP Montreal",
+      img: "https://res.cloudinary.com/mymediacreative/image/upload/c_scale,w_700/v1645733960/work/grid/POP_Header_qcj5gl.png",
+      link: "pop",
+      tag: "Video",
+    },
+    ippnwc: {
+      name: "IPPNWC",
+      img: "https://res.cloudinary.com/mymediacreative/image/upload/c_scale,w_700/v1645733960/work/grid/IPPNWC_Header_fse62a.png",
+      link: "ippnwc",
+      tag: "Campaign Strategy",
+    },
+    indigirecruit: {
+      name: "IndigIrecruit",
+      img: "https://res.cloudinary.com/mymediacreative/image/upload/c_scale,w_700/v1645733960/work/grid/IndigIrecruit_Header_pnxibt.png",
+      link: "indigirecruit",
+      tag: "Brand",
+    },
+  }
+  return (
+    <Layout navBlack={true} seo={seo} navActive="work">
+        {/*<Seo seo={homepage.attributes.seo} />*/}
+      <div className="work-head midline">
+        <div className="midline-title">
+            <svg className="circ" width="200" height="200" viewBox="0 0 200 200" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <circle cx="100" cy="100" r="100" fill="#E26E4B"/>
+            </svg>
+            <h1>Our work is tied together with <em className="dorange">passion</em> and <em className="dorange">care.</em></h1>
         </div>
-    <Footer />
-    </>
+        <div className="midline-line"> </div>
+        <div className="midline-subtitle">
+          <h5>You put your heart into making your <br /> communities, and the world, better. <br /> You deserve to work with people who <br /> <span className="dorange"> do the same.</span></h5>
+        </div>
+      </div>
+
+      <div className="work-highlight">
+          <WorkHighlight proj={content[0]} />
+      </div>
+
+      <div className="full-width">
+        {/*<div className="work-filter">
+            <ul>
+              <li className="menu-active"><a>All</a></li>
+              <li className=""><a>Project Type</a></li>
+              <li className=""><a>Impact Areas</a></li>
+              <li className=""><a>Client</a></li>
+            </ul>
+           </div>
+        <div className="work-filter-submenu">
+          <div className="work-filter-divider" />
+            <ul>
+              <li><a>Video</a></li>
+              <li><a>UI/UX Design</a></li>
+              <li><a>Web Design</a></li>
+              <li><a>Web Development</a></li>
+              <li><a>Other</a></li>
+              <li><a>Other</a></li>
+              <li><a>Other</a></li>
+              <li><a>Other</a></li>
+            </ul>
+          <div className="work-filter-divider" />
+        </div>*/}
+
+        <div className="work-grid">
+          <WorkCard proj={projs.cera} />
+          <WorkCard proj={projs.psystem} />
+          <WorkCard proj={projs.mcconnell} />
+          <WorkCard proj={projs.chasm} />
+          <WorkCard proj={projs.ECTHP} />
+          <WorkCard proj={projs.nofish} />
+          <WorkCard proj={projs.pop} />
+          <WorkCard proj={projs.ippnwc} />
+          <WorkCard proj={projs.indigirecruit} />
+        </div>
+       <Spacer h="80px" />
+      </div>
+    </Layout>
   )
-}
-
-export async function getStaticPaths() {
-  const paths = content.map(page => {
-    const slug = page.path.split('/').slice(1);
-    return {params: {slug}};
-  });
-  return {paths, fallback: true};
-}
-
-export async function getStaticProps({params}) {
-  const currentPath = `/${params.slug.join('/')}`;
-  const page = content.find(page => page.path === currentPath) || {notfound: true};
-  return {props: {page}};
-}
-
-const gallery = (g) => {
-    if(g.type == "image") return <img src={g.media} />
-    if(g.type == "loop") return <video width="100%" autoPlay={true} loop playsInline muted style={{ margin: "10px 0" }} ><source src={g.media} type="video/mp4" /></video>
-    if(g.type == "vimeo") return <ReactPlayer url={g.media} controls={true} width="" height="" />
-    if(g.type == "twoImg") return <div className="two-img">{ gallery(g.media[0]) }{ gallery(g.media[1]) }</div>
-    if(g.type == "threeImg") return <div className="three-img">{ gallery(g.media[0]) }{ gallery(g.media[1]) }{ gallery(g.media[2]) }</div>
 }
